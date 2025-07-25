003f9b1aefd8dbf2055c8703472694661dba1f9d
Rolf Louisdor <rolflouisdor@gmail.com>
2025-07-25 14:38:10 -0400

    wait-for-posgres.sh

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -4,10 +4,14 @@ set -e
 host="${DB_HOST}"
 port="${DB_PORT:-5432}"
 user="${DB_USER}"
+password="${DB_PASSWORD}"
+dbname="${DB_NAME}"
+
+export PGPASSWORD="$password"
 
 echo "Waiting for Postgres at $host:$port (user: $user)..."
 
-until pg_isready -h "$host" -p "$port" -U "$user"; do
+until psql "host=$host port=$port user=$user dbname=$dbname sslmode=require" -c '\q' > /dev/null 2>&1; do
   echo "Postgres is unavailable - sleeping"
   sleep 2
 done
